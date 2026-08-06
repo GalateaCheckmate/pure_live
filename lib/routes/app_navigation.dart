1c0a325c19e47278314bec1c258a673cad51a36e
GalateaCheckmate <jasonli4165@gmail.com>
2026-08-06 14:07:36 +0800

    fix(player): guard duplicate live-room navigation [skip ci]

--- lib/routes/app_navigation.dart
+++ lib/routes/app_navigation.dart
@@ -1,37 +1,64 @@
-import 'dart:io';
+import 'dart:async';
 import 'dart:developer';
-import 'package:pure_live/common/index.dart';
-import 'package:pure_live/plugins/utils.dart';
-import 'package:pure_live/player/utils/fullscreen.dart';
+import 'dart:io';
+
 import 'package:pure_live/common/global/platform_utils.dart';
+import 'package:pure_live/common/index.dart';
 import 'package:pure_live/modules/live_play/live_play_controller.dart';
+import 'package:pure_live/player/utils/fullscreen.dart';
+import 'package:pure_live/plugins/utils.dart';
 
 /// APP页面跳转封装
 /// * 需要参数的页面都应使用此类
 /// * 如不需要参数，可以使用Get.toNamed
 class AppNavigator {
+  static bool _openingLiveRoom = false;
+
   /// 跳转至分类详情
-  static void toCategoryDetail({required Site site, required LiveArea category}) {
+  static void toCategoryDetail({
+    required Site site,
+    required LiveArea category,
+  }) {
     Get.toNamed(RoutePath.kAreaRooms, arguments: [site, category]);
   }
 
-  /// 跳转至直播间
+  /// 跳转至直播间。同一次导航未结束时忽略重复点击，避免同时创建两个播放器页面。
   static Future<void> toLiveRoomDetail({required LiveRoom liveRoom}) async {
-    Get.toNamed(RoutePath.kLivePlay, arguments: liveRoom, parameters: {"site": liveRoom.platform!});
+    if (_openingLiveRoom) return;
+    _openingLiveRoom = true;
+    try {
+      await Get.toNamed(
+        RoutePath.kLivePlay,
+        arguments: liveRoom,
+        parameters: {'site': liveRoom.platform!},
+      );
+    } finally {
+      _openingLiveRoom = false;
+    }
   }
 
-  static Future<void> offAndToRoomDetail({required LiveRoom liveRoom}) async {
-    Get.offAndToNamed(RoutePath.kLivePlay, arguments: liveRoom, parameters: {"site": liveRoom.platform!});
+  static Future<void> offAndToRoomDetail({
+    required LiveRoom liveRoom,
+  }) async {
+    await Get.offAndToNamed(
+      RoutePath.kLivePlay,
+      arguments: liveRoom,
+      parameters: {'site': liveRoom.platform!},
+    );
   }
 
   /// 跳转至哔哩哔哩登录
-  static Future toBiliBiliLogin() async {
-    var contents = [i18n("sms_login"), i18n("qrcode_login")];
+  static Future<void> toBiliBiliLogin() async {
+    final contents = [i18n('sms_login'), i18n('qrcode_login')];
     if (Platform.isAndroid || Platform.isIOS) {
-      var result = await Utils.showOptionDialog(contents, '', title: i18n("select_login_method"));
-      if (result == i18n("sms_login")) {
+      final result = await Utils.showOptionDialog(
+        contents,
+        '',
+        title: i18n('select_login_method'),
+      );
+      if (result == i18n('sms_login')) {
         await Get.toNamed(RoutePath.kBiliBiliWebLogin);
-      } else if (result == i18n("qrcode_login")) {
+      } else if (result == i18n('qrcode_login')) {
         await Get.toNamed(RoutePath.kBiliBiliQRLogin);
       }
     } else {
@@ -42,34 +69,33 @@ class AppNavigator {
 
 class BackButtonObserver extends RouteObserver<PageRoute<dynamic>> {
   @override
-  void didPop(Route route, Route? previousRoute) {
+  void didPop(Route<dynamic> route, Route<dynamic>? previousRoute) {
     super.didPop(route, previousRoute);
-    if (route.settings.name == RoutePath.kLivePlay) {
-      try {
-        final livePlayController = Get.find<LivePlayController>();
-        livePlayController.success.value = false;
+    if (route.settings.name != RoutePath.kLivePlay) return;
 
-        final manager = GlobalPlayerService.instance.playerManager;
-        if (SettingsService.to.player.floatPlay.v) {
-          Future.delayed(Duration(milliseconds: 200), () {
-            manager.showAppFloating();
-          });
+    try {
+      final livePlayController = Get.find<LivePlayController>();
+      livePlayController.success.value = false;
+
+      final manager = GlobalPlayerService.instance.playerManager;
+      if (SettingsService.to.player.floatPlay.v) {
+        Future<void>.delayed(const Duration(milliseconds: 200), () async {
+          manager.showAppFloating();
+        });
+      } else {
+        livePlayController.videoController.value?.clearListener();
+        if (livePlayController.isCurrentRoomAudioOnly.value) {
+          unawaited(manager.hardDispose());
         } else {
-          if (livePlayController.videoController.value != null) {
-            livePlayController.videoController.value?.clearListener();
-          }
-          if (livePlayController.isCurrentRoomAudioOnly.value) {
-            manager.hardDispose();
-          } else {
-            manager.close();
-          }
+          unawaited(manager.close());
         }
-        if (PlatformUtils.isMobile) {
-          WindowService().doExitFullScreen();
-        }
-      } catch (e) {
-        log("BackButtonObserver Error: ${e.toString()}");
       }
+
+      if (PlatformUtils.isMobile) {
+        WindowService().doExitFullScreen();
+      }
+    } catch (error) {
+      log('BackButtonObserver Error: $error');
     }
   }
 
@@ -77,8 +103,7 @@ class BackButtonObserver extends RouteObserver<PageRoute<dynamic>> {
   void didPush(Route<dynamic> route, Route<dynamic>? previousRoute) {
     super.didPush(route, previousRoute);
     if (route.settings.name == RoutePath.kLivePlay) {
-      final manager = GlobalPlayerService.instance.playerManager;
-      manager.closeAppFloating();
+      GlobalPlayerService.instance.playerManager.closeAppFloating();
     }
   }
 }
